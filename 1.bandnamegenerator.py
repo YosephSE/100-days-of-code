4f546f831d7aa2e29ee1f2d34b10e8f91bb1237d
YosephSE <yoseph.kedir10@gmail.com>
2023-02-15 19:50:48 +0300

    day 1 file finished

---
+++ 1.bandnamegenerator.py
@@ -0,0 +1,5 @@
+print('Here is your band name generator. \n please follow the program to get your band name.')
+city = input('Enter the city you grew up in ?\n')
+pet = input('Enter the name of the per you have? \n')
+print(f'Your band name should be {city} {pet}')
+
